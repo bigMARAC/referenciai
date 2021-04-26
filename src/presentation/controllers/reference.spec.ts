c44613119219a93e06d440f7bdc35d3b02814976
Marcos Emanuel <marcos.emanuel@dataclick.com.br>
2021-04-26 16:32:36 -0300

    test: ensure ReferenceController returns 400 if an invalid name is provided

--- src/presentation/controllers/reference.spec.ts
+++ src/presentation/controllers/reference.spec.ts
@@ -1,13 +1,30 @@
 import { ReferenceController } from './reference'
 import { MissingParamError } from '../errors/missing-param-error'
+import { InvalidParamError } from '../errors/invalid-param-error'
+import { NameValidator } from '../protocols/name-validator'
 
-const makeSut = (): ReferenceController => {
-  return new ReferenceController()
+interface SutTypes {
+  sut: ReferenceController
+  nameValidatorStub: NameValidator
+}
+
+const makeSut = (): SutTypes => {
+  class NameValidatorStub implements NameValidator {
+    isValid (name: string): boolean {
+      return true
+    }
+  }
+  const nameValidatorStub = new NameValidatorStub()
+  const sut = new ReferenceController(nameValidatorStub)
+  return {
+    sut,
+    nameValidatorStub
+  }
 }
 
 describe('Reference Controller', () => {
-  test('Should return 400 if no title is provided ', () => {
-    const sut = makeSut()
+  test('Should return 400 if no title is provided', () => {
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         author: 'Marcos Emanuel',
@@ -21,8 +38,8 @@ describe('Reference Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('title'))
   })
-  test('Should return 400 if no author is provided ', () => {
-    const sut = makeSut()
+  test('Should return 400 if no author is provided', () => {
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         title: 'Any Title',
@@ -35,5 +52,22 @@ describe('Reference Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('author'))
+  })
+  test('Should return 400 if an invalid name is provided', () => {
+    const { sut, nameValidatorStub } = makeSut()
+    jest.spyOn(nameValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        author: 'Invalid-Name',
+        title: 'Any Title',
+        subtitle: 'Any Subtitle',
+        edition: '1',
+        place: 'Any Place',
+        date: '26/04/2021'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('author'))
   })
 })
