918f0f6d3b1d46c424e55c3074dd7e4da687d251
Marcos Emanuel <marcos.emanuel@dataclick.com.br>
2021-04-26 16:57:05 -0300

    feat: ensure ReferenceController returns 500 if NameValidator throws

--- src/presentation/controllers/reference.ts
+++ src/presentation/controllers/reference.ts
@@ -4,6 +4,7 @@ import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { NameValidator } from '../protocols/name-validator'
 import { InvalidParamError } from '../errors/invalid-param-error'
+import { ServerError } from '../errors/server-error'
 
 export class ReferenceController implements Controller {
   private readonly nameValidator: NameValidator
@@ -12,15 +13,22 @@ export class ReferenceController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const required = ['title', 'author']
-    for (const field of required) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const required = ['title', 'author']
+      for (const field of required) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
-    const isValid = this.nameValidator.isValid(httpRequest.body.author)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('author'))
+      const isValid = this.nameValidator.isValid(httpRequest.body.author)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('author'))
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
