187000b317b24a5716369e2293cf375922d9d17d
Marcos Emanuel <marcos.emanuel@dataclick.com.br>
2021-04-26 15:51:25 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/reference.ts
+++ src/presentation/controllers/reference.ts
@@ -1,20 +1,15 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 
 export class ReferenceController {
   handle (httpRequest: HttpRequest): HttpResponse {
     const { title, author } = httpRequest.body
     if (!title) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('title')
-      }
+      return badRequest(new MissingParamError('title'))
     }
     if (!author) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('author')
-      }
+      return badRequest(new MissingParamError('author'))
     }
   }
 }
