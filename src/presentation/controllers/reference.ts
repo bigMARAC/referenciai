edc5f45128a8c2bf9e72996af76707d26d2aa419
Marcos Emanuel <marcos.emanuel@dataclick.com.br>
2021-04-26 16:32:13 -0300

    feat: ensure ReferenceController returns 400 if an invalid name is provided

--- src/presentation/controllers/reference.ts
+++ src/presentation/controllers/reference.ts
@@ -2,14 +2,25 @@ import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
+import { NameValidator } from '../protocols/name-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
 
 export class ReferenceController implements Controller {
+  private readonly nameValidator: NameValidator
+  constructor (nameValidator: NameValidator) {
+    this.nameValidator = nameValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const required = ['title', 'author']
     for (const field of required) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    const isValid = this.nameValidator.isValid(httpRequest.body.author)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('author'))
     }
   }
 }
