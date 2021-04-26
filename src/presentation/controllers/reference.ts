838eec64f4e4a6552bd3d81e7c0701bccad85a6e
Marcos Emanuel <marcos.emanuel@dataclick.com.br>
2021-04-26 15:29:02 -0300

    feat: ensure ReferenceController returns an error if no author is provided

--- src/presentation/controllers/reference.ts
+++ src/presentation/controllers/reference.ts
@@ -1,8 +1,17 @@
 export class ReferenceController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: title')
+    const { title, author } = httpRequest.body
+    if (!title) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: title')
+      }
+    }
+    if (!author) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: author')
+      }
     }
   }
 }
