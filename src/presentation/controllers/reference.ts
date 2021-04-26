06555af42fa6a0c7fae18f91e8198b9555dc8fd4
Marcos Emanuel <marcos.emanuel@dataclick.com.br>
2021-04-26 15:54:00 -0300

    refactor: simplify code

--- src/presentation/controllers/reference.ts
+++ src/presentation/controllers/reference.ts
@@ -4,12 +4,11 @@ import { badRequest } from '../helpers/http-helper'
 
 export class ReferenceController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const { title, author } = httpRequest.body
-    if (!title) {
-      return badRequest(new MissingParamError('title'))
-    }
-    if (!author) {
-      return badRequest(new MissingParamError('author'))
+    const required = ['title', 'author']
+    for (const field of required) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
