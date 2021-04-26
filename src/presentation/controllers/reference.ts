4093c41fdb7e6329c52583aeb5db0cc905aa67cb
Marcos Emanuel <marcos.emanuel@dataclick.com.br>
2021-04-26 15:23:02 -0300

    feat: ensure ReferenceController returns an error if no title is provided

--- src/presentation/controllers/reference.ts
+++ src/presentation/controllers/reference.ts
@@ -1,7 +1,8 @@
 export class ReferenceController {
   handle (httpRequest: any): any {
     return {
-      statusCode: 400
+      statusCode: 400,
+      body: new Error('Missing param: title')
     }
   }
 }
