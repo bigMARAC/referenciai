9c6e603ef51d77675f3387719e7b2a636641e39c
bigMARAC <marcos.length@gmail.com>
2021-04-27 17:43:55 -0300

    test: ensure DbCreateReference throws if Maker throws

--- src/data/usecases/create-reference/db-create-reference.spec.ts
+++ src/data/usecases/create-reference/db-create-reference.spec.ts
@@ -42,4 +42,19 @@ describe('DbCreateReference', () => {
     await sut.add(referenceData)
     expect(makeSpy).toHaveBeenCalledWith(referenceData)
   })
+  test('Should throw if Maker throws', async () => {
+    const { sut, makerStub } = makeSut()
+    jest.spyOn(makerStub, 'make').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const referenceData = {
+      author: 'Marcos Emanuel',
+      title: 'Title',
+      subtitle: 'Subtitle',
+      edition: '1',
+      place: 'Place',
+      company: 'Company',
+      date: '26/04/2021'
+    }
+    const promise = sut.add(referenceData)
+    await expect(promise).rejects.toThrow()
+  })
 })
