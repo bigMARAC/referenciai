4ab0ab1f666f9b525170ffbaf36c2e573120f1f2
bigMARAC <marcos.length@gmail.com>
2021-04-26 22:26:11 -0300

    test: ensure ReferenceController calls CreateReference with correct values

--- src/presentation/controllers/reference.spec.ts
+++ src/presentation/controllers/reference.spec.ts
@@ -1,6 +1,7 @@
 import { ReferenceController } from './reference'
 import { MissingParamError, InvalidParamError, ServerError } from '../errors'
 import { NameValidator } from '../protocols'
+import { CreateReference, CreateReferenceModel } from '../../domain/usecases/create-reference'
 
 const makeNameValidator = (): NameValidator => {
   class NameValidatorStub implements NameValidator {
@@ -11,17 +12,30 @@ const makeNameValidator = (): NameValidator => {
   return new NameValidatorStub()
 }
 
+const makeCreateReference = (): CreateReference => {
+  class CreateReferenceStub implements CreateReference {
+    add (account: CreateReferenceModel): string {
+      const fakeReference = 'EMANUEL, Marcos.'
+      return fakeReference
+    }
+  }
+  return new CreateReferenceStub()
+}
+
 interface SutTypes {
   sut: ReferenceController
   nameValidatorStub: NameValidator
+  createReferenceStub: CreateReference
 }
 
 const makeSut = (): SutTypes => {
   const nameValidatorStub = makeNameValidator()
-  const sut = new ReferenceController(nameValidatorStub)
+  const createReferenceStub = makeCreateReference()
+  const sut = new ReferenceController(nameValidatorStub, createReferenceStub)
   return {
     sut,
-    nameValidatorStub
+    nameValidatorStub,
+    createReferenceStub
   }
 }
 
@@ -107,5 +121,30 @@ describe('Reference Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+  test('Should call CreateReference with correct values', () => {
+    const { sut, createReferenceStub } = makeSut()
+    const addSpy = jest.spyOn(createReferenceStub, 'add')
+    const httpRequest = {
+      body: {
+        author: 'Marcos Emanuel',
+        title: 'Any Title',
+        subtitle: 'Any Subtitle',
+        edition: '1',
+        place: 'Any Place',
+        company: 'Any Company',
+        date: '26/04/2021'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(addSpy).toHaveBeenCalledWith({
+      author: 'Marcos Emanuel',
+      title: 'Any Title',
+      subtitle: 'Any Subtitle',
+      edition: '1',
+      place: 'Any Place',
+      company: 'Any Company',
+      date: '26/04/2021'
+    })
   })
 })
