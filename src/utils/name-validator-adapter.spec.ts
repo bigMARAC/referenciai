cf1bdb1e5259709bbb525e51cc292f6cb5e3ddb5
bigMARAC <marcos.length@gmail.com>
2021-04-27 16:38:08 -0300

    test: ensure NameValidatorAdapter calls validator with correct name

--- src/utils/name-validator-adapter.spec.ts
+++ src/utils/name-validator-adapter.spec.ts
@@ -1,9 +1,34 @@
-import { NameValidatorAdapter } from './name-validator'
+import { NameValidatorAdapter } from './name-validator-adapter'
+
+interface SutTypes {
+  sut: NameValidatorAdapter
+  validator: RegExp
+}
+
+const makeSut = (): SutTypes => {
+  const validator = /^[A-Z][a-z\u00C0-\u00FF']+(\s[A-Z][a-z\u00C0-\u00FF']+)+$/
+  const sut = new NameValidatorAdapter(validator)
+  return {
+    sut,
+    validator
+  }
+}
 
 describe('NameValidatorAdapter', () => {
   test('Should return false if validator returns false', () => {
-    const sut = new NameValidatorAdapter()
+    const { sut } = makeSut()
     const isValid = sut.isValid('Invalid-name')
     expect(isValid).toBe(false)
+  })
+  test('Should return false if validator returns false', () => {
+    const { sut } = makeSut()
+    const isValid = sut.isValid('Valid Name')
+    expect(isValid).toBe(true)
+  })
+  test('Should call validator with correct name', () => {
+    const { sut, validator } = makeSut()
+    const testSpy = jest.spyOn(validator, 'test')
+    sut.isValid('Valid Name')
+    expect(testSpy).toHaveBeenCalledWith('Valid Name')
   })
 })
