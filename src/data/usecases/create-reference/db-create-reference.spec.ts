6efb8bc8357b875248b2b8f6246cef650108aed4
bigMARAC <marcos.length@gmail.com>
2021-04-27 17:36:39 -0300

    refactor: move sut creation to a factory helper method

--- src/data/usecases/create-reference/db-create-reference.spec.ts
+++ src/data/usecases/create-reference/db-create-reference.spec.ts
@@ -1,15 +1,30 @@
 import { CreateReferenceModel } from '../../../domain/usecases/create-reference'
+import { Maker } from '../../protocols/maker'
 import { DbCreateReference } from './db-create-reference'
+
+interface SutTypes {
+  sut: DbCreateReference
+  makerStub: Maker
+}
+
+const makeSut = (): SutTypes => {
+  class MakerStub {
+    async make (data: CreateReferenceModel): Promise<string> {
+      return await new Promise(resolve => resolve('reference'))
+    }
+  }
+  const makerStub = new MakerStub()
+  const sut = new DbCreateReference(makerStub)
+
+  return {
+    sut,
+    makerStub
+  }
+}
 
 describe('DbCreateReference', () => {
   test('Should call Maker with correct values', async () => {
-    class MakerStub {
-      async make (data: CreateReferenceModel): Promise<string> {
-        return await new Promise(resolve => resolve('reference'))
-      }
-    }
-    const makerStub = new MakerStub()
-    const sut = new DbCreateReference(makerStub)
+    const { sut, makerStub } = makeSut()
     const makeSpy = jest.spyOn(makerStub, 'make')
     const referenceData = {
       author: 'Marcos Emanuel',
