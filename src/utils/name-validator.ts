420967c470d93271df8eb110611c2d2333e6589a
Marcos Emanuel <marcos.emanuel@dataclick.com.br>
2021-04-27 15:31:11 -0300

    test: ensure NameValidatorAdapter returns true if validator returns true

--- src/utils/name-validator.ts
+++ src/utils/name-validator.ts
@@ -2,6 +2,7 @@ import { NameValidator } from '../presentation/protocols/name-validator'
 
 export class NameValidatorAdapter implements NameValidator {
   isValid (name: string): boolean {
-    return false
+    const validator = /^[A-Z][a-z\u00C0-\u00FF']+(\s[A-Z][a-z\u00C0-\u00FF']+)+$/
+    return validator.test(name)
   }
 }
